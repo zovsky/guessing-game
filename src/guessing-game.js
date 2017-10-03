df0b6cbfdd69802d8b016f8c634d0745cc3616a1
zovsky <zovsky@gmail.com>
2017-10-03 23:51:51 +0300

    Revert "implement guessing game class"
    This reverts commit ae4ed013c93e5b9eb8e48af4d5496bcb76fba4e9.

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,24 +1,20 @@
 class GuessingGame {
-    constructor() {
-        thin.min = null;
-        this.max = null;
-    }
+    constructor() {}
 
     setRange(min, max) {
-        this.min = min;
-        this.max = max;
+
     }
 
     guess() {
-        return Math.floor(this.min + (this.max - this.min)/2);
+
     }
 
     lower() {
-        this.max = this.guess();
+
     }
 
     greater() {
-        this.min = this.guess();
+
     }
 }
 
